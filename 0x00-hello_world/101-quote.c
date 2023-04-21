fda41af6dc8fd6fbf85c176b557310079075df43
saadiaerr <saadiaerraiss1998@gmail.com>
2023-04-20 19:56:33 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unisted.h>
+#include <unistd.h>
 /**
 * main - Entry
 * Return: 1
 */
 int main(void)
 {
-	write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n"stdout);
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
 return (1);
 }
