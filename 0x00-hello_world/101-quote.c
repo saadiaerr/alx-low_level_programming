fd182129d8710a9b437b47616ab577712b4c40f2
saadiaerr <saadiaerraiss1998@gmail.com>
2023-04-20 18:53:22 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unisted.h>
+/**
+* main - Entry
+* Return: 1
+*/
+int main(void)
+{
+	fput(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n"stdout);
+return (1);
+}
