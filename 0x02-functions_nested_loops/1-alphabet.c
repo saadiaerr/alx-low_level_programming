96516069a28a668a5d9e0a4048f2f2248e247d0b
saadiaerr <saadiaerraiss1998@gmail.com>
2023-04-25 16:00:22 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	int lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		_putchar(lc);
+	_putchar('\n');
+}
