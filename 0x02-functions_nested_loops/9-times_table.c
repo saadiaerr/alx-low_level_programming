ec2ea735cd5cc21565c8960d720e5a187e10c680
saadiaerr <saadiaerraiss1998@gmail.com>
2023-04-26 05:49:47 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,30 +7,30 @@ void times_table(void)
 	int num;
 	int mult;
 	int prd;
+	int u;
+	int d;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (mult = 0; mult < 10; mult++)
+		for (mult = 0; mult <= 9; mult++)
 		{
 			prd = num * mult;
-			if (prd == 0)
+			if (prd > 9)
 			{
-				_putchar(prd + '0');
+				u = prd % 10;
+				d = (prd - u) / 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(d + '0');
+				_putchar(u + '0');
 			}
-
-			if (prd < 10 && mult != 0)
+			else if (mult != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(prd + '0');
-			}
-			else if (prd >= 10)
-			{
-				_putchar(',');
 				_putchar(' ');
-				_putchar((prd / 10) + '0');
-				_putchar((prd % 10) + '0');
 			}
+			_putchar(prd + '0');
 		}
 		_putchar('\n');
 	}
