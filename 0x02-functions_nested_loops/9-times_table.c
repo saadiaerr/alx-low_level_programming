07090249cfda5e4bcb70d6a74c6c0e9a1bd1fbf1
saadiaerr <saadiaerraiss1998@gmail.com>
2023-04-26 06:05:15 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,8 +7,6 @@ void times_table(void)
 	int num;
 	int mult;
 	int prd;
-	int u;
-	int d;
 
 	for (num = 0; num <= 9; num++)
 	{
@@ -17,19 +15,19 @@ void times_table(void)
 			prd = num * mult;
 			if (prd > 9)
 			{
-				u = prd % 10;
-				d = (prd - u) / 10;
 				_putchar(',');
 				_putchar(' ');
-				_putchar(d + '0');
-				_putchar(u + '0');
+				_putchar((prd / 10) + '0');
+				_putchar((prd % 10) + '0');
 			}
-			else if (mult != 0)
+
+			if (mult != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 			}
+			else
 			_putchar(prd + '0');
 		}
 		_putchar('\n');
