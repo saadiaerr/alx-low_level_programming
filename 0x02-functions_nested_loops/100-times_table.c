3fd91c26586934f5f54fc33de0d3f243f04c2816
saadiaerr <saadiaerraiss1998@gmail.com>
2023-04-26 08:45:10 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
   * print_times_table - prints the n times table, starting with 0
   * @n: input number to check
@@ -12,32 +11,37 @@ void print_times_table(int n)
 	{
 		for (num = 0; num <= n; num++)
 		{
-			_putchar(0);
-
-			for (mult = 1; mult <= n; mult++)
+			for (mult = 0; mult <= n; mult++)
 			{
-				_putchar(',');
-				_putchar(' ');
-
 				pr = num * mult;
 
-				if (pr <= 9)
-					_putchar(' ');
-
-				if (pr <= 99)
-				_putchar(' ');
-
-				if (pr >= 100)
+				if (pr == 0)
 				{
-				_putchar((pr / 100) + 0);
-				_putchar((pr / 10) % 10 + 0);
+					_putchar(pr + '0');
+				} else if (pr < 10 && mult != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(pr + '0');
+				} else if (pr >= 10 && pr < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((pr / 10) + '0');
+					_putchar((pr % 10) + '0');
+				} else if (pr >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((pr / 100) + '0');
+					_putchar((pr / 10) % 10 + '0');
+					_putchar((pr % 10) + '0');
 				}
-				else if (pr <= 99 && pr >= 10)
-				_putchar((pr / 10) + 0);
-				_putchar((pr % 10) + 0);
 
-			}
-			_putchar('\n');
+			} _putchar('\n');
 		}
 	}
 }
