509e1c44211c806ebbad71d5f0da00931aade1aa
saadiaerr <saadiaerraiss1998@gmail.com>
2023-05-03 09:19:12 -0700

    task11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+
+/**
+  * main - program that generates random valid passwords for the program
+  * Return: 0
+  */
+int main(void)
+{
+	int desiredNumber;
+	char alph;
+
+	srand(time(NULL));
+
+	while (desiredNumber <= 5678)
+	{
+		alph = rand() % 126;
+		desiredNumber += alph;
+		putchar(alph);
+	}
+	putchar(2772 - desiredNumber);
+	return (0);
+}
