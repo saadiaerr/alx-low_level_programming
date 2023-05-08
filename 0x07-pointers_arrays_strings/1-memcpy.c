5e7d4772a3640b711ea314d0fff5f1428a3fba2a
saadiaerr <saadiaerraiss1998@gmail.com>
2023-05-08 09:17:36 -0700

    task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,9 +11,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int y = 0;
 
-	for (y < n; y++)
+	while (y < n)
 	{
-		src[y] = dest[y];
+		dest[y] = src[y];
+		y++;
 	}
 	return (dest);
 
