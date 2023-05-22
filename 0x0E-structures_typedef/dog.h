7d78a41b9129138482d9a1181071905dafcd525a
saadiaerr <saadiaerraiss1998@gmail.com>
2023-05-22 04:47:57 -0700

    tasks

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Define a new type struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the owner of dog
+ */
+struct dog
+{
+	float age;
+
+	char *owner;
+
+	char *name;
+
+};
+
+#endif
