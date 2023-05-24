18c20d117decae85d3ac80806a334c9cff75f693
saadiaerr <saadiaerraiss1998@gmail.com>
2023-05-24 14:07:24 -0700

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
- * print_name - function that print name using pointer to function
+ * print_name - function that print name using pointer f
  * @name: string name
  * @f: pointer
  * Return: nothing
  **/
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (!name && !f)
 		return;
 
 	f(name);
