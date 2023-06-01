993bcb22a120cda6cb3d4a155e5d2da3a2aee14c
kenzalch <kenzalachgarensa@gmail.com>
2023-06-01 08:32:03 -0700

    task 101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+section .text
+	extern printf
+	global main
+
+
+	section .data
+	fmt db	"%s", 10, 0
+	msg db	"Hello, Holberton", 0
+
+main:
+	push	rbp
+	mov	rsi, msg
+	mov	rdi, fmt
+	mov	rax, 0
+	call 	printf
+	pop	rbp
+	mov	rax, 0
+	ret
