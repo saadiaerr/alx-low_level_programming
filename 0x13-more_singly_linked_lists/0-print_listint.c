a0a34f9c123088802bf10d21459c4e5c56b66541
kenzalch <kenzalachgarensa@gmail.com>
2023-06-04 22:29:38 -0700

    task0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a list
+ * @h: input value to check
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t Num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		Num++;
+		h = h->next;
+	}
+
+	return (Num);
+}
