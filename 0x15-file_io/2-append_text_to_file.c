7c45084bde21fa2e8f3ecb4f865517d81ced81be
saadiaerr <saadiaerraiss1998@gmail.com>
2023-06-12 16:38:40 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,29 +9,25 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	register int _file, _w, text_lentgh = 0;
+	register int _open, _write, text_lentgh = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-	_file = open(filename, O_WRONLY, O_APPEND);
-	if (_file == -1)
-	{
-		return (-1);
-	}
-	if (text_content)
+
+	if (text_content != NULL)
 	{
 		while (text_content[text_lentgh])
-		{
 			text_lentgh++;
-		}
-		_w = write(_file, text_content, text_lentgh);
-		if (_w == -1)
-		{
-			return (-1);
-		}
 	}
-	close(_file);
+
+	_open = open(filename, O_WRONLY | O_APPEND);
+	_write = write(_open, text_content, text_lentgh);
+
+	if (_open == -1 || _write == -1)
+		return (-1);
+
+	close(_open);
+
 	return (1);
+
 }
